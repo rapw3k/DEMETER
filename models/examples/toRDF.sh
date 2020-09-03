229964505f1498ac83bb8184dec9852ebc78fd29
Raul Palma <rapw2k@yahoo.com>
2020-09-03 12:02:31 +0200

    rdf for protege loading

---
+++ models/examples/toRDF.sh
@@ -0,0 +1,10 @@
+/Users/rap/Downloads/Utils/apache-jena-3.16.0/bin/riot --out=RDF/XML /Users/rap/GitRepositories/GitHub/DEMETER/models/agriFeature.ttl > agriFeature.rdf
+/Users/rap/Downloads/Utils/apache-jena-3.16.0/bin/riot --out=RDF/XML /Users/rap/GitRepositories/GitHub/DEMETER/models/agriAlert.ttl > agriAlert.rdf
+/Users/rap/Downloads/Utils/apache-jena-3.16.0/bin/riot --out=RDF/XML /Users/rap/GitRepositories/GitHub/DEMETER/models/agriCommon.ttl > agriCommon.rdf
+/Users/rap/Downloads/Utils/apache-jena-3.16.0/bin/riot --out=RDF/XML /Users/rap/GitRepositories/GitHub/DEMETER/models/agriCrop.ttl > agriCrop.rdf
+/Users/rap/Downloads/Utils/apache-jena-3.16.0/bin/riot --out=RDF/XML /Users/rap/GitRepositories/GitHub/DEMETER/models/agriIntervention.ttl > agriIntervention.rdf
+/Users/rap/Downloads/Utils/apache-jena-3.16.0/bin/riot --out=RDF/XML /Users/rap/GitRepositories/GitHub/DEMETER/models/agriPest.ttl > agriPest.rdf
+/Users/rap/Downloads/Utils/apache-jena-3.16.0/bin/riot --out=RDF/XML /Users/rap/GitRepositories/GitHub/DEMETER/models/agriProduct.ttl > agriProduct.rdf
+/Users/rap/Downloads/Utils/apache-jena-3.16.0/bin/riot --out=RDF/XML /Users/rap/GitRepositories/GitHub/DEMETER/models/agriProperty.ttl > agriProperty.rdf
+/Users/rap/Downloads/Utils/apache-jena-3.16.0/bin/riot --out=RDF/XML /Users/rap/GitRepositories/GitHub/DEMETER/models/agriSystem.ttl > agriSystem.rdf
+/Users/rap/Downloads/Utils/apache-jena-3.16.0/bin/riot --out=RDF/XML /Users/rap/GitRepositories/GitHub/DEMETER/models/farmAnimal.ttl > farmAnimal.rdf
