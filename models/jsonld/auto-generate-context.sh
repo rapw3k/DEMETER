ed3c5851c8fbb9dd3304bb20baece05b5e3f2e09
Raul Palma <rapw2k@yahoo.com>
2023-03-15 15:57:10 +0100

    updates

--- models/jsonld/auto-generate-context.sh
+++ models/jsonld/auto-generate-context.sh
@@ -1,16 +1,15 @@
-* MAKE SURe to SAVE version too first v2.0
-
+# NOTE: generate RDF (run toRDF.sh first
 java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o crossDomain-context.jsonld https://w3id.org/demeter/crossDomain
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o agriCommon-context.jsonld https://w3id.org/demeter/agri/agriCommon/2.0
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o agriFeature-context.jsonld https://w3id.org/demeter/agri/agriFeature/2.0
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o agriAlert-context.jsonld https://w3id.org/demeter/agri/agriAlert/2.0
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o agriCrop-context.jsonld https://w3id.org/demeter/agri/agriCrop/2.0
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o agriIntervention-context.jsonld https://w3id.org/demeter/agri/agriIntervention/2.0
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o agriPest-context.jsonld https://w3id.org/demeter/agri/agriPest/2.0
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o agriProduct-context.jsonld https://w3id.org/demeter/agri/agriProduct/2.0
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o agriProperty-context.jsonld https://w3id.org/demeter/agri/agriProperty/2.0
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o agriSystem-context.jsonld https://w3id.org/demeter/agri/agriSystem/2.0
-java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o farmAnimal-context.jsonld https://w3id.org/demeter/agri/farmAnimal/2.0
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o agriCommon-context.jsonld https://w3id.org/demeter/agri/agriCommon #https://raw.githubusercontent.com/rapw3k/DEMETER/master/models/agriCommon.ttl
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o agriFeature-context.jsonld https://w3id.org/demeter/agri/agriFeature 
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o agriAlert-context.jsonld https://w3id.org/demeter/agri/agriAlert
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o agriCrop-context.jsonld https://w3id.org/demeter/agri/agriCrop
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o agriIntervention-context.jsonld https://w3id.org/demeter/agri/agriIntervention
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o agriPest-context.jsonld https://w3id.org/demeter/agri/agriPest
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o agriProduct-context.jsonld https://w3id.org/demeter/agri/agriProduct
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o agriProperty-context.jsonld https://w3id.org/demeter/agri/agriProperty
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o agriSystem-context.jsonld https://w3id.org/demeter/agri/agriSystem
+java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -v -o farmAnimal-context.jsonld https://w3id.org/demeter/agri/farmAnimal
 
 java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o livestockFeature-context.jsonld https://w3id.org/demeter/agri/ext/livestockFeature
 java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o poultryFeeding-context-xxx.jsonld https://w3id.org/demeter/agri/ext/poultryFeeding
@@ -19,16 +18,287 @@ java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld
 java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o transportCondition-context.jsonld https://w3id.org/demeter/agri/ext/transportCondition
 java -jar /users/rap/GitRepositories/GitHub/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar -o nutrientMonitor-context.jsonld https://w3id.org/demeter/agri/ext/nutrientMonitor
 
----generating extensions
-* save ttl, commit and push
-* generate jsonld
-* Remove the following terms, and double check by comparing with demeterAgriProfile-context.jsonld that shared terms are the same, 
+
+
+####### GENERATE EXTENSIONS GUIDELINES
+: '
+1. SAVE ttl, COMMIT and PUSH
+2. GENERATE jsonld
+3. REMOVE the following terms, and double check by comparing with demeterAgriProfile-context.jsonld that shared terms are the same, 
     "name" : {
       "@id" : "http://xmlns.com/foaf/0.1/name"
     },
-    
     ,
     "description" : {
       "@id" : "http://purl.org/dc/terms/description"
     }
-* save jsonld, commit and push    
+4. SAVE jsonld, COMMIT and PUSH    
+'
+
+######### cross-domain postprocessing
+## CHANGE in generated context
+: '
+		"foaf.name" : {
+      "@id" : "http://xmlns.com/foaf/0.1/name"
+		}
+		"wgs84.Point" : {
+      "@id" : "http://www.w3.org/2003/01/geo/wgs84_pos#Point"
+    }   
+		"ssn.Property" : {
+      "@id" : "http://www.w3.org/ns/ssn/Property"
+    }    
+		"dct.description" : {
+      "@id" : "http://purl.org/dc/terms/description"
+    }    
+		"qb.Observation" : {
+      "@id" : "http://purl.org/linked-data/cube#Observation"
+    }
+		"qu.QuantityKind" : {
+      "@id" : "http://purl.oclc.org/NET/ssnx/qu/qu#QuantityKind"
+    }
+
+## ADD THE FOLLOWING TERMS    
+
+    "sf.Geometry" : {
+      "@id" : "http://www.opengis.net/ont/sf#Geometry"
+    },
+    "schema.Person" : {
+      "@id" : "https://schema.org/Person"
+    },
+    "Observation" : {
+      "@id" : "http://www.w3.org/ns/sosa/Observation"
+    },
+    "Point" : {
+      "@id" : "http://www.opengis.net/ont/sf#Point"
+    },   
+    "schema.name" : {
+      "@id" : "https://schema.org/name"
+    },
+    "QuantityKind" : {
+      "@id" : "http://qudt.org/schema/qudt/QuantityKind"
+    }
+'
+                         
+######## agriCommon             
+## CHANGE in generated context
+: '
+		"schema.name" : {
+      "@id" : "https://schema.org/name"
+    }
+		"typeName" : {
+      "@id" : "http://foodie-cloud.com/model/foodie#type",
+      "@type" : "http://www.w3.org/2001/XMLSchema#string"
+    }
+		"propertyHasValue" : {
+      "@id" : "https://saref.etsi.org/core/hasValue" (prev: "https://w3id.org/saref#hasValue")
+    },
+## ADD THE FOLLOWING TERMS        
+   "name" : {
+      "@id" : "https://smartdatamodels.org/name"
+    }
+'    
+
+######## agriFeature             
+## CHANGE in generated context
+: '
+"foaf.name" : {
+      "@id" : "http://xmlns.com/foaf/0.1/name"
+    }
+    
+## ADD THE FOLLOWING TERMS            
+   "locationGeoJson" : {
+      "@id" : "https://uri.etsi.org/ngsi-ld/location",
+      "@type" : "@id"
+    }
+    "containsSite" : {
+      "@id" : "http://inspire.ec.europa.eu/schemas/af/3.0#contains",
+      "@type" : "@id"
+    } 
+'    
+
+######## agriAlert
+## CHANGE in generated context
+: '
+		"foaf.name" : {
+      "@id" : "http://xmlns.com/foaf/0.1/name"
+    }
+		"alertValidFrom" : {
+      "@id" : "https://smartdatamodels.org/dataModel.Weather/validFrom",
+      "@type" : "http://www.w3.org/2001/XMLSchema#dateTime"
+    }        
+		"alertValidTo" : {
+      "@id" : "https://smartdatamodels.org/dataModel.Weather/validTo",
+      "@type" : "http://www.w3.org/2001/XMLSchema#dateTime"
+    }        
+		"typeName" : {
+      "@id" : "http://foodie-cloud.com/model/foodie#type",
+      "@type" : "http://www.w3.org/2001/XMLSchema#string"
+    }
+    
+## ADD THE FOLLOWING TERMS            
+   "validTo" : {
+      "@id" : "http://portele.de/ont/inspire/baseInspire#validTo",
+      "@type" : "http://www.w3.org/2001/XMLSchema#dateTime"
+    },
+    "validFrom" : {
+      "@id" : "http://portele.de/ont/inspire/baseInspire#validFrom",
+      "@type" : "http://www.w3.org/2001/XMLSchema#dateTime"
+    } 
+'   
+
+######## agriCrop
+## CHANGE in generated context
+: '
+		"foaf.name" : {
+      "@id" : "http://xmlns.com/foaf/0.1/name"
+    }
+    
+## ADD THE FOLLOWING TERMS            
+   
+'   
+
+######## agriIntervention
+## CHANGE in generated context
+: '
+		"foaf.name" : {
+      "@id" : "http://xmlns.com/foaf/0.1/name"
+    }    
+    "totalQuantity" : {
+      "@id" : "https://smartdatamodels.org/dataModel.Agrifood/quantity"
+    }
+    "typeName" : {
+      "@id" : "http://foodie-cloud.com/model/foodie#type",
+      "@type" : "http://www.w3.org/2001/XMLSchema#string"
+    }
+    
+## ADD THE FOLLOWING TERMS            
+   "operationStatus" : {
+      "@id" : "https://uri.etsi.org/ngsi-ld/status"
+    }
+    "quantity" : {
+      "@id" : "http://foodie-cloud.com/model/foodie#quantity",
+      "@type" : "@id"
+    }
+'   
+
+######## agriPest
+## CHANGE in generated context
+: '
+		"foaf.name" : {
+      "@id" : "http://xmlns.com/foaf/0.1/name"
+    }
+    "dct.description" : {
+      "@id" : "http://purl.org/dc/terms/description"
+    }
+    
+## ADD THE FOLLOWING TERMS            
+   
+'  
+
+######## agriProduct
+## CHANGE in generated context
+: '
+		"composedPrimarilyOf" : {
+      "@id" : "http://purl.obolibrary.org/obo/RO_0002473",
+      "@type" : "@id"
+    }
+    "hasCountryOfOrigin" : {
+      "@id" : "http://purl.obolibrary.org/obo/HANCESTRO_0308",
+      "@type" : "@id"
+    }
+    "hasFoodSubstanceAnalog" : {
+      "@id" : "http://purl.obolibrary.org/obo/FOODON_00001301",
+      "@type" : "@id"
+    }
+    "hasComponent" : {
+      "@id" : "http://purl.obolibrary.org/obo/RO_0002180",
+      "@type" : "@id"
+    }
+    
+## ADD THE FOLLOWING TERMS            
+   
+'   
+
+######## agriProperty
+## CHANGE in generated context
+: '
+		"propertyHasValue" : {
+      "@id" : "https://saref.etsi.org/core/hasValue"
+    }
+    "saref.hasFeatureOfInterest" : {
+      "@id" : "https://saref.etsi.org/core/hasFeatureOfInterest",
+      "@type" : "@id"
+    }
+    "cf.Surface" : {
+      "@id" : "http://purl.oclc.org/NET/ssnx/cf/cf-feature#Surface"
+    }
+    "dct.description" : {
+      "@id" : "http://purl.org/dc/terms/description"
+    }
+## ADD THE FOLLOWING TERMS   
+    "saref.Temperature" : {
+      "@id" : "https://saref.etsi.org/core/Temperature"
+    }
+    "saref.FeatureOfInterest" : {
+      "@id" : "https://saref.etsi.org/core/FeatureOfInterest"
+    }
+    "saref.hasProperty" : {
+      "@id" : "https://saref.etsi.org/core/hasProperty",
+      "@type" : "@id"
+    }
+    "saref.isPropertyOf" : {
+      "@id" : "https://saref.etsi.org/core/isPropertyOf",
+      "@type" : "@id"
+    }
+    "hasFeatureOfInterest" : {
+      "@id" : "http://www.w3.org/ns/sosa/hasFeatureOfInterest",
+      "@type" : "@id"
+    }
+    "saref.isFeatureOfInterestOf" : {
+      "@id" : "https://saref.etsi.org/core/isFeatureOfInterestOf",
+      "@type" : "@id"
+    }
+   
+'  
+
+######## agriSystem
+## CHANGE in generated context
+: '
+		"foaf.name" : {
+      "@id" : "http://xmlns.com/foaf/0.1/name"
+    }
+    "dct.description" : {
+      "@id" : "http://purl.org/dc/terms/description"
+    }
+    
+## ADD THE FOLLOWING TERMS            
+   "saref.Sensor" : {
+      "@id" : "https://saref.etsi.org/core/Sensor"
+    }
+'  
+
+######## farmAnimal
+## CHANGE in generated context
+: '
+		"animalSpecies" : {
+      "@id" : "https://smartdatamodels.org/dataModel.Agrifood/species"
+    }
+    "isMemberOfAnimalGroup" : {
+      "@id" : "https://saref.etsi.org/saref4agri/isMemberOf",
+      "@type" : "@id"
+    }
+    "foaf.name" : {
+      "@id" : "http://xmlns.com/foaf/0.1/name"
+    }
+    "hasMemberAnimal" : {
+      "@id" : "https://saref.etsi.org/saref4agri/hasMember",
+      "@type" : "@id"
+    }
+    "dct.description" : {
+      "@id" : "http://purl.org/dc/terms/description"
+    }
+## ADD THE FOLLOWING TERMS            
+   "FarmAnimal" : {
+      "@id" : "https://smartdatamodels.org/dataModel.Agrifood/Animal"
+    }
+'  
